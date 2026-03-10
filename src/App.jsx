e1b88841518c064ab9948684bd33e1dd6b574965
park3hho <ejfhr7777@gmail.com>
2026-03-10 21:55:56 +0900

    0.3.3 Process Pages

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,23 @@ import RMPage from './pages/RMPage'
 export default function App() {
   const { user, loading, error, login, logout } = useAuth()
 
-  const PAGE_MAP = {
-    ADM: <PrintPage onLogout={logout} />,
-    RM: <RMPage onLogout={logout} />,
-  }
+const PAGE_MAP = {
+  RM:  <RMPage onLogout={logout} />,   // 1
+  MP:  <MPPage onLogout={logout} />,   // 2
+  EA:  <EAPage onLogout={logout} />,   // 3
+  HT:  <HTPage onLogout={logout} />,   // 4
+  BO:  <BOPage onLogout={logout} />,   // 5
+  EC:  <ECPage onLogout={logout} />,   // 6
+  IQ:  <IQPage onLogout={logout} />,   // 7
+  WI:  <WIPage onLogout={logout} />,   // 8
+  SO:  <SOPage onLogout={logout} />,   // 9
+  OQ:  <OQPage onLogout={logout} />,   // 10
+  BOX: <BOXPage onLogout={logout} />,  // 11
+  ADM: <PrintPage onLogout={logout} />, // 12
+}
 
   return user
     ? PAGE_MAP[user.process_type] ?? <PrintPage onLogout={logout} />
     : <LoginPage onLogin={login} loading={loading} error={error} />
 }
+
