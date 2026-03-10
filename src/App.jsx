45d90c4e53465e1a3b5d34614de6442a58403572
park3hho <ejfhr7777@gmail.com>
2026-03-10 18:02:41 +0900

    0.3.2 logout

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,15 @@ import { LoginPage } from './pages/LoginPage'
 import { PrintPage } from './pages/PrintPage'
 import RMPage from './pages/RMPage'
 
-const PAGE_MAP = {
-  ADM: <PrintPage />,
-  RM: <RMPage />,
-}
-
 export default function App() {
   const { user, loading, error, login, logout } = useAuth()
 
+  const PAGE_MAP = {
+    ADM: <PrintPage onLogout={logout} />,
+    RM: <RMPage onLogout={logout} />,
+  }
+
   return user
-    ? PAGE_MAP[user.process_type] ?? <PrintPage />
+    ? PAGE_MAP[user.process_type] ?? <PrintPage onLogout={logout} />
     : <LoginPage onLogin={login} loading={loading} error={error} />
 }
