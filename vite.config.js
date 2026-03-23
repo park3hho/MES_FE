f981319dfee0c19678b5ec9f450e835e6bffc14d
park3hho <ejfhr7777@gmail.com>
2026-03-23 09:54:59 +0900

    v0.5.6_30 LOGO RESIZE

--- vite.config.js
+++ vite.config.js
@@ -17,8 +17,8 @@ export default defineConfig({
         display_override: ['window-controls-overlay'],
         start_url: '/',
         icons: [
-          { src: 'pwa-192x192.png', sizes: '192x192', type: 'image/png' },
-          { src: 'pwa-512x512.png', sizes: '512x512', type: 'image/png', purpose: 'any maskable' }
+          { src: 'pwa-192x192.png', sizes: '192x192', type: 'image/png', purpose: 'any' },        // 홈화면 아이콘용
+          { src: 'pwa-512x512.png', sizes: '512x512', type: 'image/png', purpose: 'maskable' },   // 스플래시용
         ]
       },
       workbox: {
